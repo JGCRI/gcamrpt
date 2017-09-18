bb88ce1887919d65c6a0e71323878cd19c00e392
xavier-gutierrez <xaviersgutierrez@gmail.com>
2017-09-18 11:12:24 -0400

    Moved count unit conversion function to units.R

--- R/socioeconomics.R
+++ R/socioeconomics.R
@@ -27,23 +27,8 @@ module.population <- function(mode, allqueries, aggkeys, aggfn, strtyr, endyr,
 
         population <- allqueries$'Population'
         population <- aggregate(population, aggfn, aggkeys)
-        population <- filter(population, startyr, endyr, filters)
+        population <- filter(population, strtyr, endyr, filters)
         population <- unitconv_counts(population, ounit)
         population
     }
-}
-
-
-#' Unit conversion: counts
-#'
-#' This function converts data reported in counts, e.g. thousands, millions, etc..
-#'
-#' @param ounit Desired output unit.  If omitted, results will be returned with
-#' no unit conversion.
-#' @keywords internal
-unitconv <- function(module_data, ounit)
-{
-    #Needs work
-    message("Counts unit conversion function not yet implemented. Data returned unmodified")
-    module_data
 }
