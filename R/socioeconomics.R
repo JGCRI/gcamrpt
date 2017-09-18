d541c7c3dffc3a57616dd0e9cd1b5e6c5262f499
xavier-gutierrez <xaviersgutierrez@gmail.com>
2017-09-18 11:57:36 -0400

    GDP PPP and MER data processing module, and entries in variable.ctl

--- R/socioeconomics.R
+++ R/socioeconomics.R
@@ -32,3 +32,70 @@ module.population <- function(mode, allqueries, aggkeys, aggfn, strtyr, endyr,
         population
     }
 }
+
+#' GDP MER Data Module
+#'
+#' Produce GDP MER by region.
+#'
+#' Columns:
+#' \itemize{
+#'   \item{region}
+#'   \item{year}
+#'   \item{value}
+#'   \item{units}
+#' }
+#'
+#' @keywords internal
+
+module.gdp_mer <- function(mode, allqueries, aggkeys, aggfn, strtyr, endyr,
+                              filters, ounit)
+{
+    if(mode == GETQ) {
+        # Return titles of necessary queries
+        # For more complex variables, will return multiple query titles.
+        'GDP MER'
+    }
+    else {
+        message('Function for processing variable: GDP MER')
+
+        gdp_mer <- allqueries$'gdp_mer'
+        gdp_mer <- aggregate(gdp_mer, aggfn, aggkeys)
+        gdp_mer <- filter(gdp_mer, strtyr, endyr, filters)
+        gdp_mer <- unitconv_counts(gdp_mer, ounit)
+        gdp_mer
+    }
+}
+
+#' GDP PPP Data Module
+#'
+#' Produce GDP PPP by region.
+#'
+#' Columns:
+#' \itemize{
+#'   \item{region}
+#'   \item{year}
+#'   \item{value}
+#'   \item{units}
+#' }
+#'
+#' @keywords internal
+
+module.gdp_ppp <- function(mode, allqueries, aggkeys, aggfn, strtyr, endyr,
+                           filters, ounit)
+{
+    if(mode == GETQ) {
+        # Return titles of necessary queries
+        # For more complex variables, will return multiple query titles.
+        'GDP PPP'
+    }
+    else {
+        message('Function for processing variable: GDP PPP')
+
+        gdp_ppp <- allqueries$'gdp_ppp'
+        gdp_ppp <- aggregate(gdp_ppp, aggfn, aggkeys)
+        gdp_ppp <- filter(gdp_ppp, strtyr, endyr, filters)
+        gdp_ppp <- unitconv_counts(gdp_ppp, ounit)
+        gdp_ppp
+    }
+}
+
--- inst/extdata/variable.ctl
+++ inst/extdata/variable.ctl
@@ -1,3 +1,5 @@
 GCAM variable,output variable,aggregation keys,aggregation function,start year,end year,filters,output units
 Population,Population,,,1990,2020,,thous
-Electricity,Electricity,,,,,,
+Electricity,Electricity,,,,,,EJ
+GDP(PPP),gdp_ppp,,,,,,Thous90US$/per
+GDP(MER),gdp_mer,,,,,,Thous90US$/per
