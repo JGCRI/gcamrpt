43cadb5e2ee706eafb92c72ec972bf5033b849cf
xavier-gutierrez <xaviersgutierrez@gmail.com>
2017-09-18 19:13:03 -0400

    Change 'value' and 'units' column as entries of list, not dataframe

--- R/units.R
+++ R/units.R
@@ -42,36 +42,36 @@ unitconv_usdollar <- function(module_data, ounit)
 {
     if (length(unique(module_data$Units)) > 1) {
         warning(paste("Variable reported in multiple units"))
-    }
-    else {
-        iunit <- gdp_ppp$Units[1]
+    } else if (module_data$Units[1] != ounit) {
+        iunit <- module_data$Units[1]
         year_pattern <- "[0-9]{2}"
 
         # extract input year
-        m <- regexpr(pattern, iunit)
+        m <- regexpr(year_pattern, iunit)
         input_year <- paste("19", regmatches(iunit, m), sep="")
 
         # regexp extract output year
-        m <- regexpr(pattern, ounit)
+        m <- regexpr(year_pattern, ounit)
         output_year <- paste("19", regmatches(ounit, m), sep="")
 
         # take GDPDEF data. average over each year
         gdpdef <- system.file("extdata", "GDPDEF.csv", package="iamrpt")
         gdpdef <- read.table(gdpdef, header=TRUE, sep=",", as.is=TRUE)
-        gdpdef$DATE = as.POSIXlt(gdpdef$DATE, format = "%Y-%m-%d") #convert to POSIXct class
-        gdpdef$year = year(gdpdef$DATE) # add year column
-        gdpdef.grp.year = gdpdef[c("GDPDEF", "year")] %>%
+        gdpdef$DATE <- as.POSIXlt(gdpdef$DATE, format = "%Y-%m-%d") #convert to POSIXct class
+        gdpdef$year <- year(gdpdef$DATE) # add year column
+        gdpdef.grp.year <- gdpdef[c("GDPDEF", "year")] %>%
             group_by(year) %>%
-            summarize(avg=mean(GDPDEF))
-        gdpdef.grp.year = data.frame(gdpdef.grp.year) #return to data.frame (tibble after pipe operations)
+            summarize(avg = mean(GDPDEF))
+        gdpdef.grp.year<- data.frame(gdpdef.grp.year) #return to data.frame (tibble after pipe operations)
 
         # calculate conversion factor
-        conv_factr <- gdpdef.grp.year[which(gdpdef.grp.year$year == output_year), avg] / gdpdef.grp.year[which(gdpdef.grp.year$year == input_year), avg]
+        conv_factr <- gdpdef.grp.year[which(gdpdef.grp.year$year == output_year), 'avg'] / gdpdef.grp.year[which(gdpdef.grp.year$year == input_year), 'avg']
 
         # unit conversion
-        module_data$value <- conv_factr*module_data$value
+        module_data[['value']] <- module_data[['value']] * conv_factr
 
         # rename units column
-        module_data$Units <- ounit
+        module_data[['Units']] <- rep(ounit, length(module_data[['Units']]))
+
     }
 }
