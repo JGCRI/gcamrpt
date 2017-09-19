80c3245686113971f14b337b29b49de17d7e0033
xavier-gutierrez <xaviersgutierrez@gmail.com>
2017-09-19 16:33:07 -0400

    Energy unit conversion

--- R/units.R
+++ R/units.R
@@ -25,9 +25,29 @@ unitconv_counts <- function(module_data, ounit)
 #' @keywords internal
 unitconv_energy <- function(module_data, ounit)
 {
-    #Needs work
-    message("Energy unit conversion function not yet implemented. Data returned unmodified")
+    if(is.null(ounit) || is.na(ounit)) {
+        return(module_data)
+    }
+    assert_that(length(ounit) == 1)
+    assert_that(is.character(ounit))
+
+    if (length(unique(module_data$Units)) > 1) {
+        ## We'll accommodate this case as best we can, but there is something
+        ## squirrelly about this data.
+        warning("Avast! This variable be reported in multiple units.  Arrr!")
+    }
+    else {
+        iunit <- module_data$Units[1]
+    }
+
+    cfacs <- system.file("extdata", "energyconv.csv", package="iamrpt")
+    cfacs <- utils::read.csv(cfacs, header=TRUE, row.names=1, sep=",")
+    cfac <- cfacs[iunit, ounit]
+
+    module_data[['value']] <- module_data[['value']] * cfac
+
+    ## Update the units
+    module_data[['Units']] <- ounit
+
     module_data
-
-
 }
