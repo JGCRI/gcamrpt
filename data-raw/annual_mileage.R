ad286c60912687119f445260565d152678a8fbaa
xavier-gutierrez <xaviersgutierrez@gmail.com>
2017-10-13 15:38:37 -0400

    Fix df naming typo

--- data-raw/annual_mileage.R
+++ data-raw/annual_mileage.R
@@ -3,14 +3,14 @@
 
 calc.annual_mileage <- function(filename) {
     # read in data
-    calc.annual_mileage <- readr::read_csv(filename)
+    annual_mileage <- readr::read_csv(filename)
 
-    calc.annual_mileage <- tidyr::gather(calc.annual_mileage,
+    annual_mileage <- tidyr::gather(annual_mileage,
                                           key=year ,
                                           value=value,
                                           -service, -mode, -submode, -Units)
-    calc.annual_mileage$year <- as.integer(calc.annual_mileage$year) # in order to join with other df's
+    annual_mileage$year <- as.integer(annual_mileage$year) # in order to join with other df's
 
-    calc.annual_mileage
+    annual_mileage
 
 }
