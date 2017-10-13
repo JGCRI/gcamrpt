4b750d9b5627fb5b39153f5a73ece4f5af318c5a
xavier-gutierrez <xaviersgutierrez@gmail.com>
2017-10-13 17:27:53 -0400

    Fixed count conv matrix

--- data-raw/countconv.R
+++ data-raw/countconv.R
@@ -1,8 +1,8 @@
 prep.countconv <- function()
 {
-    fromBillion  <- c(1   , 1/1e3, 1/1e6   )
-    fromMillion  <- c(1e3 , 1    , 1/1e3   )
-    fromThousand <- c(1e6 , 1e3  , 1       )
+    fromBillion  <- c(1     , 1e3    , 1e6   )
+    fromMillion  <- c(1/1e3 , 1      , 1e3   )
+    fromThousand <- c(1/1e6 , 1/1e3  , 1     )
     cm <- matrix(c(fromBillion, fromMillion, fromThousand), nrow=3, byrow=TRUE)
     colnames(cm) <- c('billion', 'million', 'thousand')
     row.names(cm) <- colnames(cm)
