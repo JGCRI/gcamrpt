847c52114173bef4a5a4c62cf528015df6e7d8f6
xavier-gutierrez <xaviersgutierrez@gmail.com>
2017-10-17 13:40:55 -0400

    Use normalize() function in variable calculation

--- R/emissions.R
+++ R/emissions.R
@@ -26,11 +26,10 @@ module.co2_emissions <- function(mode, allqueries, aggkeys, aggfn, years,
     else {
         message('Function for processing variable: CO2 emissions')
         co2 <- allqueries$'CO2 emissions'
-        # sometimes appears in query. useless info
-        if ('rundate' %in% names(co2)) {co2 <- dplyr::select(co2, -rundate)}
-
-        co2 <- semiaggregate(co2)
-        co2 <- parse_sector(co2, hasvintage=FALSE, hasfuel=FALSE, hastechnology=FALSE)
+        co2 <- normalize(co2) %>% # function stored in transp modules group
+            dplyr::group_by(Units, scenario, region, year, service, mode, submode) %>%
+            dplyr::summarise(value=sum(value)) %>%
+            dplyr::ungroup()
         co2 <- filter(co2, years, filters)
         co2 <- aggregate(co2, aggfn, aggkeys)
         co2 <- unitconv_co2(co2, ounit)
@@ -38,7 +37,7 @@ module.co2_emissions <- function(mode, allqueries, aggkeys, aggfn, years,
     }
 }
 
-#' CO2 emissions Data Module
+#' PM emissions Data Module
 #'
 #' Produce service output by technology and vintage
 #'
@@ -63,26 +62,15 @@ module.pm_emissions <- function(mode, allqueries, aggkeys, aggfn, years,
     }
     else {
         message('Function for processing variable: PM emissions')
-        serviceOutput <- allqueries$'Service output'
-        ldfctr <- allqueries$'Load factors'
-        # sometimes appears in query. useless info
-        if ('rundate' %in% names(serviceOutput)) {serviceOutput <- dplyr::select(serviceOutput, -rundate)}
-        if ('rundate' %in% names(ldfctr)) {ldfctr <- dplyr::select(ldfctr, -rundate)}
-        # query output includes NA column
-        ldfctr <- ldfctr[, !(names(ldfctr) %in% c('load-factor'))]
-
         # data prep
-        serviceOutput <- vint_tech_split(serviceOutput)
-        serviceOutput <- parse_sector(serviceOutput, hasvintage=TRUE, hasfuel=FALSE, hastechnology=TRUE)
-        serviceOutput <- serviceOutput %>% # sum over vintage/technology
-            dplyr::group_by(Units, scenario, region, service, mode, submode, year) %>%
+        serviceOutput <- allqueries$'Service output'
+        serviceOutput <- normalize(serviceOutput) %>%
+            dplyr::group_by(Units, scenario, region, year, service, mode, submode) %>%
             dplyr::summarise(value=sum(value)) %>%
             dplyr::ungroup()
-
-        ldfctr <- semiaggregate(ldfctr)
-        ldfctr <- parse_sector(ldfctr, hasvintage=FALSE, hasfuel=FALSE, hastechnology=TRUE)
-        ldfctr <- ldfctr %>% # average over technology (same for all submodes, so really just collapsing technology column)
-            dplyr::group_by(Units, scenario, region, service, mode, submode, year) %>%
+        ldfctr <- allqueries$'Load factors'
+        ldfctr <- normalize(ldfctr) %>%
+            dplyr::group_by(Units, scenario, region, year, service, mode, submode) %>% # average over LHDT
             dplyr::summarise(value=mean(value)) %>%
             dplyr::ungroup()
 
