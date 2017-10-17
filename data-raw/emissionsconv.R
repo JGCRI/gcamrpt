dc90266fd45368df0bc5fdbbd2a504e3fe19bb4c
xavier-gutierrez <xaviersgutierrez@gmail.com>
2017-10-17 13:41:47 -0400

    Change 'mt co2' to 'mtco2' so compsplit() unit function recognizes as one unit

--- data-raw/emissionsconv.R
+++ data-raw/emissionsconv.R
@@ -3,7 +3,7 @@ prep.emissionsconv <- function()
     fromMTC <-  c(1,        42/12)
     fromMTCO2 <- c(12/42, 1     )
     cm <- matrix(c(fromMTC, fromMTCO2), nrow=2, byrow=TRUE)
-    colnames(cm) <- c('mtc', 'mt co2')
+    colnames(cm) <- c('mtc', 'mtco2')
     row.names(cm) <- colnames(cm)
 
     cm
